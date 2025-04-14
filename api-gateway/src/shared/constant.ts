23f3e7f43d8374f30d41db8f4dc9ab8ec11217ae
pulkit-khullar <pulkitkhullar15@gmail.com>
2025-04-15 01:23:08 +0530

    setup revers proxy

---
+++ api-gateway/src/shared/constant.ts
@@ -0,0 +1,8 @@
+import { RouteMapping } from "./interfaces";
+
+export const dynamicRoutesMapping: RouteMapping[] = [
+    {
+        routeName: '/auth',
+        routePath: 'http://localhost:8001'
+    }
+]
---
+++ api-gateway/src/shared/interfaces.ts
@@ -0,0 +1,4 @@
+export interface RouteMapping{
+    routeName: string,
+    routePath: string
+}
