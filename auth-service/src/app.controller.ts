452890290917973cb286c47c42dac42a0801733c
pulkit-khullar <pulkitkhullar15@gmail.com>
2025-04-20 17:36:57 +0530

    feat/add-centralised-logging-error-handling

--- auth-service/src/app.controller.ts
+++ auth-service/src/app.controller.ts
@@ -1,13 +1,33 @@
-import { Controller, Get, Header, Headers } from '@nestjs/common';
+import { Controller, Get, Header, Headers, Logger, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
-@Controller()
+@Controller('/auth')
 export class AppController {
+  logger: Logger = new Logger(AppController.name);
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/hello')
   getHello(@Headers('x-api-gateway') apiGatewayHeader: string): string {
-    console.log(apiGatewayHeader);
     return this.appService.getHello();
+  }
+
+  @Get('/error')
+  async getError(
+    @Headers('x-api-gateway') apiGatewayHeader: string,
+    @Res() res: Response,
+  ): Promise<void> {
+    try {
+      const data = this.appService.getError();
+      res.status(200).send();
+    } catch (error) {
+      this.logger.error(error.message);
+      res.status(500).json({ message: error.message });
+    }
+  }
+
+  @Get('/unhandled-error')
+  async getUnhandledError(): Promise<void> {
+    this.appService.getError();
   }
 }
--- auth-service/src/main.ts
+++ auth-service/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  /**
+   * Setting baseurl for all the API end-points in the application
+   */
+  app.setGlobalPrefix(configService.get<string>('API_BASEPATH') || '');
+
+  /**
+   * Pick application port from ENV
+   */
+  await app.listen(configService.get<number>('PORT') ?? 3000);
 }
 bootstrap();
